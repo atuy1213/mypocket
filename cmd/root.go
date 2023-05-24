e0490d357496ef30deee2bb17f45d73e1b921e54
Yuta Yoshimura <atuy1213@gmail.com>
2023-05-24 19:14:46 +0900

    hide log loading config file

--- cmd/root.go
+++ cmd/root.go
@@ -23,6 +23,7 @@ package cmd
 
 import (
 	"fmt"
+	"log"
 	"os"
 
 	"github.com/spf13/cobra"
@@ -88,7 +89,7 @@ func initConfig() {
 	viper.AutomaticEnv() // read in environment variables that match
 
 	// If a config file is found, read it in.
-	if err := viper.ReadInConfig(); err == nil {
-		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
+	if err := viper.ReadInConfig(); err != nil {
+		log.Fatalln(err)
 	}
 }
